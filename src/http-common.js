2f3edf0f0ad2d64cecfab34d381c2ebd34424594
Deniz <denzdonmez@gmail.com>
2022-03-25 10:17:02 +0300

    electron app client

--- src/http-common.js
+++ src/http-common.js
@@ -1,8 +1,8 @@
 import axios from "axios";
 
 export default axios.create({
-  baseURL: "http://localhost:8080/api",
+  baseURL: "http://10.68.19.149:8080/api",
   headers: {
-    "Content-type": "application/json"
-  }
+    "Content-type": "application/json",
+  },
 });
--- src/services/api.js
+++ src/services/api.js
@@ -1,7 +1,7 @@
 import axios from "axios";
 
 const instance = axios.create({
-  baseURL: "http://localhost:8080/api",
+  baseURL: "http://10.68.19.149:8080/api",
   headers: {
     "Content-Type": "application/json",
   },
